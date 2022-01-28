f45515984e23a11b8e74c9d05180c285e2bf632c
bogdan-edu <bogdan-education@gmail.com>
2022-01-28 09:15:56 +0200

    add new sample

---
+++ input.py
@@ -0,0 +1,7 @@
+user_data = input("Введіть число: ")
+# user_data = int(user_data)
+user_data = float(user_data)
+second_data = 10
+result = user_data + second_data
+
+print("Результат: ", result)
---
+++ strings.py
@@ -0,0 +1,8 @@
+my_text1 = "Привіт, "
+my_text2 = "семикласники"
+
+result = my_text1 + my_text2
+print(result)
+
+result = my_text1 * 10
+print(result)
